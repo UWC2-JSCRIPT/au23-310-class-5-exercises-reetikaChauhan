a174af03943f26997d9c837d1559b23cfbfa061a
Reetika <chauhanreetika45@gmail.com>
2023-11-09 17:51:45 -0800

    Accessing.js , dom-crud.js in-class excercises completed

--- accessing.js
+++ accessing.js
@@ -1,5 +1,13 @@
 // Change the text of the "Seattle Weather" header to "February 10 Weather Forecast, Seattle"
+const header_text = document.getElementById('weather-head')
+header_text.innerText = "February 10 Weather Forecast, Seattle"
 
 // Change the styling of every element with class "sun" to set the color to "orange"
-
+const sunobj = document.querySelectorAll(".sun")
+sunobj.forEach((element) => {
+        element.style.color = 'orange'
+});
 // Change the class of the second <li> from to "sun" to "cloudy"
+const links = document.getElementsByTagName("ul")[0]
+const lis = links.getElementsByTagName("li")[1]
+lis.classList.replace("sun","cloudy")
--- plusesAndMinuses.js
+++ plusesAndMinuses.js
@@ -1,2 +1,14 @@
 // When a user clicks the + element, the count should increase by 1 on screen.
 // When a user clicks the – element, the count should decrease by 1 on screen.
+let counter = 0
+const plus = document.getElementById("plus")
+const minus = document.getElementById("minus")
+
+function incdecr(inc){
+    if (inc){
+        counter++;
+    }
+    else{
+        counter--;
+    }
+}
