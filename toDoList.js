633349f5bf714a02ff323d4cdd100a4a31bc3fca
Reetika <chauhanreetika45@gmail.com>
2023-11-11 15:13:12 -0800

    Implemented todo list, traversing, updated plusminus.js

--- toDoList.js
+++ toDoList.js
@@ -1,6 +1,13 @@
 // If an li element is clicked, toggle the class "done" on the <li>
-
+const liEl = document.getElementsByTagName("li")[0]
+console.log(liEl)
+liEl.classList.toggle("done")
 // If a delete link is clicked, delete the li element / remove from the DOM
+const deleteEl = document.querySelectorAll(".delete")[0]
+console.log(deleteEl)
+deleteEl.addEventListener('click',(e) => {
+    liEl.parentNode.removeChild(liEl)
+})
 
 // If an 'Add' link is clicked, adds the item as a new list item with
 // addListItem function has been started to help you get going!
@@ -9,6 +16,27 @@ const addListItem = function(e) {
   e.preventDefault();
   const input = this.parentNode.getElementsByTagName('input')[0];
   const text = input.value; // use this text to create a new <li>
+  // creating new li element
+  const textnode = document.createTextNode(text)
+  const newliEl = document.createElement("li")
+  const spanEl = document.createElement("span")
+  spanEl.appendChild(textnode)
+  const anchorEl = document.createElement("a")
+  anchorEl.setAttribute("class","delete")
+  const anchortext = document.createTextNode("Delete")
+  anchorEl.appendChild(anchortext)
+  //appending anchor tag and span to new list element
+  newliEl.append(spanEl,anchorEl)
+  // appending new li element to parent ul element
+  const parentLi = document.getElementsByTagName("ul")[0]
+  parentLi.appendChild(newliEl)
+  // new event listener on delete link
+  anchorEl.addEventListener("click",(e) =>{
+  parentLi.removeChild(newliEl)
+  })
 
   // Finish function here
 };
+const addEl = document.querySelectorAll(".add-item")[0]
+console.log(addEl)
+addEl.addEventListener("click",addListItem)
--- traversing.js
+++ traversing.js
@@ -1,11 +1,20 @@
 // Given the <body> element as variable body,
 // access the <main> node and log to the console.
 const body = document.querySelector('body');
+const mainEl = body.children[0]
+console.log(mainEl)
 
 // Given the <ul> element as variable ul,
 // access <body>  and log to the console.
 const ul = document.querySelector('ul');
+const parentoful = ul.parentNode // parentNode  of <ul> is <main>
+const bodyEl = parentoful.parentNode // parentNode of <main> is <body>
+console.log(bodyEl)
 
 // Given the <p> element as var p,
 // access the 3rd <li>  and log to the console.
 const p = document.querySelector('p');
+const p_parentNode = p.parentNode  // parent Node of p is <main>
+const ulEle = p_parentNode.children[1] // one of children of <main> is ul
+const liEle = ulEle.children[2] // children of <ul> are <li> tags
+console.log(liEle)
