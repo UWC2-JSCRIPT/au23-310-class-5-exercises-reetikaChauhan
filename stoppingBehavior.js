d18a18365dd51f169df361025410896529c2883b
Reetika <chauhanreetika45@gmail.com>
2023-11-12 14:21:18 -0800

    stoppping beahaviour.js implemented

--- stoppingBehavior.js
+++ stoppingBehavior.js
@@ -5,6 +5,19 @@ document.getElementById('cat').addEventListener('click', () => {
 
 // When clicked, "More info" link should alert "Here's some info"
 // instead of going to a new webpage
+const aEle = document.getElementById("more-info")
+aEle.setAttribute("href"," ")
+aEle.addEventListener("click",() => {
+  const text = `Here's some info`
+  alert(text)
+})
 
 // When the bark button is clicked, should alert "Bow wow!"
 // Should *not* alert "meow"
+
+
+
+document.getElementById("dog").addEventListener('click', (e) => {
+  e.stopPropagation();
+  alert("Bow wow!")
+});
